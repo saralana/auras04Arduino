5c5cf898e13d7555e6202d8723159071a10b788b
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-11-23 02:33:04 -0200

    servo motor

--- auras04Arduino.ino
+++ auras04Arduino.ino
@@ -1,69 +1,102 @@
+#include <Servo.h> 
+
 #define ANALOG_IN A5
-
-#define THRESH 100
-#define ONTIME   3000
-#define ONDELAY  0
-
+#define THRESH   500
+#define ONTIME   1500
+#define ONDELAY  1500
 
 // state variables
 int ledState;
 unsigned long lastOn;
 unsigned long toTurnOn;
+//Buttons
+int cameraButton = 0; 
+int closeButton = 0;
+Servo myservo; 
 
 void setup() {
-  // setup
+  // setup  
+  myservo.attach(7);  
   pinMode(13,OUTPUT);
-
+  //digitalWrite(ANALOG_IN, HIGH);
+  //Button
+  pinMode(2, INPUT);  
+  pinMode(4, INPUT);
   //RGB led
-  pinMode(11,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(9,OUTPUT); 
-
+  pinMode(3,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT); 
   // cathodes
-  analogWrite(9,255);  
-  analogWrite(10,255); 
-  analogWrite(11,255); 
-
-  //Serial.begin(19200);
-
+  analogWrite(3,0);  
+  analogWrite(5,0); 
+  analogWrite(6,0); 
+  Serial.begin(19200);
   // initial state
   ledState = LOW;
   lastOn = millis();
   toTurnOn = -1;
-
   // pin stuff
   digitalWrite(13,ledState);
   analogReference(DEFAULT);    // 5V  
   //analogReference(EXTERNAL);   // 
   //analogReference(INTERNAL);    // 1.1V
-
 }
-
 
 void loop() {
   // read analog in
   int val = analogRead(ANALOG_IN);
+  //cameraButton = digitalRead(4);  
+  closeButton = digitalRead(2); 
+
+  if ((closeButton == HIGH)) { 
+    digitalWrite(13,HIGH);
+    myservo.write(90); 
+    delay(1);
+    myservo.write(0);
+    delay(1000);    
+
+  }
+  else
+    digitalWrite(13,LOW); 
+  //myservo.write(0); 
 
   // if off and saw a value and haven't seen other value yet
-  //    then set up a time to turn on
+  // then set up a time to turn on
+  //if((ledState == LOW)&&(cameraButton == LOW)&&(toTurnOn == -1)){
   if((ledState == LOW)&&(val < THRESH)&&(toTurnOn == -1)){
     toTurnOn = millis()+ONDELAY;
   }
   // if off and have seen a high value and it's time to turn on
-  else if((ledState == LOW)&&(toTurnOn != -1)&&(millis() > toTurnOn)){
+  //else if((ledState == LOW)&&(cameraButton == HIGH)&&(toTurnOn != -1)&&(millis() > toTurnOn)){
+  else if((ledState == LOW)&&(val > THRESH)&&(toTurnOn != -1)&&(millis() > toTurnOn)){
+    //turn leds on
     ledState = HIGH;
-    toTurnOn = -1;
     digitalWrite(13,ledState);
-    rgb(10,11,9);
-    lastOn = millis();    
+    rgb(5,6,3,A0,A1,0);
+    delay(ONTIME/3);
+    turnOff(3,5,6);
+    rgb(5,6,3,A2,0,A3);
+    delay(ONTIME/3);
+    turnOff(3,5,6);
+    rgb(5,6,3,0,A1,A4);
+    delay(ONTIME/3);
+    //      turnOff(3,5,6);
+    toTurnOn = -1;
+    lastOn = millis();   
   }
   // if on for more than ONTIME milliseconds
   else if((ledState == HIGH)&&((millis()-lastOn) > ONTIME)){
+    //turn leds off
     ledState = LOW;
     digitalWrite(13,ledState);
-    digitalWrite(11,ledState);
-    digitalWrite(10,ledState);
-    digitalWrite(9,ledState);
+    digitalWrite(6,ledState);
+    digitalWrite(5,ledState);
+    digitalWrite(3,ledState);
+    myservo.write(0); 
+    delay(1);
+    //open camera box
+    myservo.write(90);
+    delay(1000);  
   }
 
   Serial.write( 0xff);
@@ -73,15 +106,15 @@ void loop() {
 
 
 
-void rgb(int r, int g, int b){
+void rgb(int r, int g, int b, int analog1, int analog2, int analog3){
 
   int oldRGB[3] = {
-    0,0,0        };
+    0,0,0  };
 
-  int val = analogRead(A0);
+  int val = analogRead(analog1);
   val = constrain(val,600,900);
-  if(abs(val - oldRGB[0]) > 12){
-    int mval = map(val,600,900,0,255);
+  if(abs(val - oldRGB[0]) > 5){
+    int mval = map(val,600,900,255,0);
     analogWrite(b,mval); 
     oldRGB[0] = val;
     Serial.print("B ");
@@ -90,10 +123,10 @@ void rgb(int r, int g, int b){
   }
   delay(10);
 
-  val = analogRead(A1);
+  val = analogRead(analog2);
   val = constrain(val,600,900);
-  if(abs(val - oldRGB[1]) > 12){
-    int mval = map(val,600,900,0,255);
+  if(abs(val - oldRGB[1]) > 5){
+    int mval = map(val,600,900,255,0);
     analogWrite(r,mval); 
     oldRGB[1] = val;
     Serial.print("R ");
@@ -102,10 +135,10 @@ void rgb(int r, int g, int b){
   }
   delay(10);
 
-  val = analogRead(A2);
+  val = analogRead(analog3);
   val = constrain(val,600,900);
-  if(abs(val - oldRGB[2]) > 12){
-    int mval = map(val,600,900,0,255);    
+  if(abs(val - oldRGB[2]) > 5){
+    int mval = map(val,600,900,255,0);    
     analogWrite(g,mval); 
     oldRGB[2] = val;
     Serial.print("G ");
@@ -114,6 +147,13 @@ void rgb(int r, int g, int b){
   }
   delay(10);
 
+}
+
+
+void turnOff (int r, int g, int b){
+  digitalWrite(r,LOW);
+  digitalWrite(g,LOW);
+  digitalWrite(b,LOW);
 }
 
 
