278c3cd6a84c42a25f4300507597ac5d07f406db
Thiago Hersan <thiago.hersan+github@gmail.com>
2012-11-23 02:30:51 -0200

    rgb

--- auras04Arduino.ino
+++ auras04Arduino.ino
@@ -1,60 +1,60 @@
-#define ANALOG_IN A0
 
-#define THRESH 100
-#define ONTIME   3000
-#define ONDELAY  0
+int oldRGB[3] = {
+  0,0,0};
 
-
-// state variables
-int ledState;
-unsigned long lastOn;
-unsigned long toTurnOn;
-
-void setup() {
-  // setup
-  //Serial.begin(9600);
+void setup(){
   pinMode(13,OUTPUT);
-  pinMode(12,OUTPUT);
+  pinMode(9,OUTPUT); 
+  pinMode(10,OUTPUT);
+  pinMode(11,OUTPUT);
 
-  // initial state
-  ledState = LOW;
-  lastOn = millis();
-  toTurnOn = -1;
+  // anode
+  digitalWrite(13,HIGH);
 
-  // pin stuff
-  digitalWrite(13,ledState);
-  digitalWrite(12,ledState);
-  analogReference(DEFAULT);    // 5V  
-  //analogReference(EXTERNAL);   // 
-  //analogReference(INTERNAL);    // 1.1V
+  // cathodes
+  analogWrite(9,255);  
+  analogWrite(10,255); 
+  analogWrite(11,255); 
+
+  Serial.begin(19200);
 }
 
-void loop() {
-  // read analog in
-  int val = analogRead(ANALOG_IN);
+void loop(){
+  int val = analogRead(A0);
+  val = constrain(val,600,900);
+  if(abs(val - oldRGB[0]) > 12){
+    int mval = map(val,600,900,0,255);
+    analogWrite(9,mval);
+    oldRGB[0] = val;
+    Serial.print("B ");
+    Serial.print(mval);
+    Serial.print('\n');
+  }
+  delay(10);
 
-  // if off and saw a value and haven't seen other value yet
-  //    then set up a time to turn on
-  if((ledState == LOW)&&(val < THRESH)&&(toTurnOn == -1)){
-    toTurnOn = millis()+ONDELAY;
+  val = analogRead(A1);
+  val = constrain(val,600,900);
+  if(abs(val - oldRGB[1]) > 12){
+    int mval = map(val,600,900,0,255);
+    analogWrite(10,mval);
+    oldRGB[1] = val;
+    Serial.print("R ");
+    Serial.print(mval);
+    Serial.print('\n');
   }
-  // if off and have seen a high value and it's time to turn on
-  else if((ledState == LOW)&&(toTurnOn != -1)&&(millis() > toTurnOn)){
-    ledState = HIGH;
-    toTurnOn = -1;
-    digitalWrite(13,ledState);
-    digitalWrite(12,ledState);
-    lastOn = millis();    
-  }
-  // if on for more than ONTIME milliseconds
-  else if((ledState == HIGH)&&((millis()-lastOn) > ONTIME)){
-    ledState = LOW;
-    digitalWrite(13,ledState);
-    digitalWrite(12,ledState);
-  }
+  delay(10);
 
-  Serial.write( 0xff);
-  Serial.write( (val >> 8) & 0xff);
-  Serial.write( val & 0xff);
+  val = analogRead(A2);
+  val = constrain(val,600,900);
+  if(abs(val - oldRGB[2]) > 12){
+    int mval = map(val,600,900,0,255);    
+    analogWrite(11,mval);
+    oldRGB[2] = val;
+    Serial.print("G ");
+    Serial.print(mval);
+    Serial.print('\n');
+  }
+  delay(10);
+
 }
 
