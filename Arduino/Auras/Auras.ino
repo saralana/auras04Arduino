d0f98b928d7e7488364ba2f5313b87da23719fb6
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-09-13 14:15:35 -0300

    adds pixel code

--- Arduino/Auras/Auras.ino
+++ Arduino/Auras/Auras.ino
@@ -1,20 +1,29 @@
 #include <Servo.h>
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+#include <avr/power.h>
+#endif
 
 enum Lid { OPEN = 110, CLOSED = 170 };
 
-Servo myservo;
-Lid mLid = OPEN;
-
 short SERVO_PIN = 3;
 short LID_PIN = 2;
+short PIXEL_PIN = 6;
 short SHUTTER_PIN = A0;
 short SHUTTER_THRESHOLD = 500;
+
+Servo myservo;
+Adafruit_NeoPixel mPixels = Adafruit_NeoPixel(16, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
+Lid mLid = OPEN;
 
 void setup() {
   myservo.attach(SERVO_PIN);
   pinMode(LID_PIN, INPUT_PULLUP);
+  digitalWrite(A0, INPUT_PULLUP);
   mLid = OPEN;
   myservo.write(OPEN);
+  mPixels.begin();
+  mPixels.show();
 }
 
 void loop() {
