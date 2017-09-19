6d6fffaed1bf5b8c6e99aae7b7c5f9a6bb27ea74
Thiago Hersan <thiago.hersan+github@gmail.com>
2017-09-19 02:04:25 -0300

    auras code with colors

--- Arduino/Auras/Auras.ino
+++ Arduino/Auras/Auras.ino
@@ -4,13 +4,13 @@
 #include <avr/power.h>
 #endif
 
-enum Lid { OPEN = 110, CLOSED = 170 };
+enum Lid { OPEN = 110, CLOSED = 175 };
 
 short SERVO_PIN = 3;
 short LID_PIN = 2;
 short PIXEL_PIN = 6;
 short SHUTTER_PIN = A0;
-short SHUTTER_THRESHOLD = 500;
+short SHUTTER_THRESHOLD = 300;
 
 Servo myservo;
 Adafruit_NeoPixel mPixels = Adafruit_NeoPixel(16, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
@@ -23,6 +23,7 @@ void setup() {
   mLid = OPEN;
   myservo.write(OPEN);
   mPixels.begin();
+  mPixels.setBrightness(128);
   mPixels.show();
 }
 
@@ -34,12 +35,42 @@ void loop() {
     }
   } else {
     if (analogRead(SHUTTER_PIN) < SHUTTER_THRESHOLD) {
-      // TODO: Turn on lights
-      // delay();
+      delay(100);
+
+      showQuarterPixels(mPixels, 0, 100, mPixels.Color(0, 0, 255));
+      showQuarterPixels(mPixels, 1, 100, mPixels.Color(255, 0, 0));
+      showQuarterPixels(mPixels, 2, 100, mPixels.Color(0, 255, 0));
+      showQuarterPixels(mPixels, 3, 100, mPixels.Color(255, 0, 255));
+
+      clearPixels(mPixels);
+      delay(1000);
+
       mLid = OPEN;
       myservo.write(OPEN);
       delay(10);
     }
   }
+}
+
+void clearPixels(Adafruit_NeoPixel &mPixels) {
+  for (int i = 0; i < mPixels.numPixels(); i++) {
+    mPixels.setPixelColor(i, mPixels.Color(0, 0, 0));
+  }
+  mPixels.show();
+}
+
+void showQuarterPixels(Adafruit_NeoPixel &mPixels, short mQuarter, short mDelay, uint32_t mColor) {
+  short numPixels = mPixels.numPixels();
+  for (int i = 0; i < numPixels; i++) {
+    if (i / (numPixels / 4) == mQuarter) {
+      mPixels.setPixelColor(i, mColor);
+    }
+  }
+  mPixels.show();
+  delay(mDelay);
+}
+
+short randomColor() {
+  return random(32, 255);
 }
 
