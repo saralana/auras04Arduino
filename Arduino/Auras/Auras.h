8ee6a57d440c76d740435564fcd717439a1aac0c
Thiago Hersan <thiago.hersan+github@gmail.com>
2018-03-15 15:35:01 -0300

    new pins for uno/nodemcu. refactors delays

---
+++ Arduino/Auras/Auras.h
@@ -0,0 +1,15 @@
+String OTA_HOSTNAME = "Auras-ESP";
+
+void setupAndStartOTA() {
+  ArduinoOTA.setHostname(OTA_HOSTNAME.c_str());
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+}
+
--- Arduino/Auras/Auras.ino
+++ Arduino/Auras/Auras.ino
@@ -1,50 +1,61 @@
+#include <Arduino.h>
 #include <Servo.h>
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-#include <avr/power.h>
-#endif
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ESP8266HTTPClient.h>
+#include "Auras.h"
 
-enum Lid { OPEN = 110, CLOSED = 171 };
+enum Lid { OPEN_LID = 110, CLOSED_LID = 171 };
 
-short SERVO_PIN = 3;
-short LID_PIN = 2;
-short PIXEL_PIN = 6;
+short SERVO_PIN = 4;
+short LID_PIN = 10;
+short PIXEL_PIN = 5;
 short SHUTTER_PIN = A0;
 short SHUTTER_THRESHOLD = 300;
 
+short TOTAL_EXPOSURE_MILLIS = 1700;
+short PAUSE_EXPOSURE_MILLIS = 100;
+short COLOR_EXPOSURE_MILLIS = 100;
+
 Servo myservo;
 Adafruit_NeoPixel mPixels = Adafruit_NeoPixel(16, PIXEL_PIN, NEO_GRB + NEO_KHZ800);
-Lid mLid = OPEN;
+Lid mLid = OPEN_LID;
 
 void setup() {
   myservo.attach(SERVO_PIN);
   pinMode(LID_PIN, INPUT_PULLUP);
-  digitalWrite(A0, INPUT_PULLUP);
-  mLid = OPEN;
-  myservo.write(OPEN);
+  digitalWrite(SHUTTER_PIN, INPUT_PULLUP);
+  mLid = OPEN_LID;
+  myservo.write(OPEN_LID);
   mPixels.begin();
   mPixels.setBrightness(255);
   mPixels.show();
 }
 
 void loop() {
-  if (mLid == OPEN) {
+  if (mLid == OPEN_LID) {
     if (digitalRead(LID_PIN) == LOW) {
-      mLid = CLOSED;
-      myservo.write(CLOSED);
+      mLid = CLOSED_LID;
+      myservo.write(CLOSED_LID);
     }
   } else {
     if (analogRead(SHUTTER_PIN) < SHUTTER_THRESHOLD) {
-      delay(100);
+      clearPixels(mPixels, PAUSE_EXPOSURE_MILLIS);
+      showQuarterPixels(mPixels, 0, COLOR_EXPOSURE_MILLIS, mPixels.Color(0, 0, 255));
 
-      showQuarterPixels(mPixels, 0, 30, mPixels.Color(0, 0, 255));
-      clearPixels(mPixels, 100);
-      showQuarterPixels(mPixels, 2, 30, mPixels.Color(0, 255, 0));
+      clearPixels(mPixels, PAUSE_EXPOSURE_MILLIS);
+      showQuarterPixels(mPixels, 2, COLOR_EXPOSURE_MILLIS, mPixels.Color(0, 255, 0));
 
-      clearPixels(mPixels, 1800);
+      clearPixels(mPixels, PAUSE_EXPOSURE_MILLIS);
+      showQuarterPixels(mPixels, 3, COLOR_EXPOSURE_MILLIS, mPixels.Color(255, 0, 0));
 
-      mLid = OPEN;
-      myservo.write(OPEN);
+      clearPixels(mPixels, TOTAL_EXPOSURE_MILLIS - 3 * (PAUSE_EXPOSURE_MILLIS + COLOR_EXPOSURE_MILLIS));
+
+      mLid = OPEN_LID;
+      myservo.write(OPEN_LID);
       delay(10);
     }
   }
